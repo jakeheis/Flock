9c0de2c9397963389afb7ce6029e3c7bd7288f58
Jake Heiser <jake@quizlet.com>
2015-12-30 12:07:14 -0600

    Local swiftcli

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "Flock",
   dependencies: [
-    .Package(url: "https://github.com/jakeheis/SwiftCLI.git", majorVersion: 1, minor: 1)
+    .Package(url: "/Users/jakeheiser/Documents/Apps/SwiftCLI", majorVersion: 1, minor: 1)
   ]
 )
