963038a1fc4f98ba4ea4f15ce2729f896ff1e790
Jake Heiser <jake@quizlet.com>
2015-12-31 14:52:40 -0600

    Only execute scheduled tasks when running whole cluster

--- Sources/TaskExecutor.swift
+++ Sources/TaskExecutor.swift
@@ -13,24 +13,20 @@ class TaskExecutor {
     
     func runCluster(cluster: Cluster, mode: Mode) {
         for keyedTask in cluster.keyedTasks() {
+            runTasksScheduledAtTime(.Before(keyedTask.key), mode: mode)
             runTask(keyedTask, mode: mode)
+            runTasksScheduledAtTime(.After(keyedTask.key), mode: mode)
         }
     }
     
     func runTask(keyedTask: KeyedTask, mode: Mode) {
-        runTasksScheduledAtTime(.Before(keyedTask.key), mode: mode)
-        executeTask(keyedTask, mode: mode)
-        runTasksScheduledAtTime(.After(keyedTask.key), mode: mode)
-    }
-    
-    // MARK: - Private
-    
-    private func executeTask(keyedTask: KeyedTask, mode: Mode) {
         switch mode {
         case .Execute: keyedTask.task.run()
         case .Print: print(keyedTask.key)
         }
     }
+    
+    // MARK: - Private
     
     private func runTasksScheduledAtTime(scheduleTime: ScheduleTime, mode: Mode) {
         let tasks = scheduler.scheduledTasksAtTime(scheduleTime)
