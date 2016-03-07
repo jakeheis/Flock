084f095e9a9f5a34cc0a71621c1371afe7efaaf3
Jake Heiser <jakeheiser1@gmail.com>
2016-03-07 11:53:06 -0500

    FlockRouter.swift

--- Sources/Flock.swift
+++ Sources/Flock.swift
@@ -45,33 +45,3 @@ public class Flock {
     }
     
 }
-
-class FlockRouter: RouterType {
-    
-    func route(commands: [CommandType], arguments: RawArguments) throws -> CommandType {
-        let clusterCommands = commands.flatMap { $0 as? ClusterCommand }
-        
-        guard let commandName = arguments.firstArgumentOfType(.Unclassified) else {
-            throw CLIError.Error("Cluster router failed")
-        }
-        
-        let clusterName: String
-        let taskName: String?
-        if let colonIndex = commandName.characters.indexOf(":") {
-            clusterName = commandName.substringToIndex(colonIndex)
-            taskName = commandName.substringFromIndex(colonIndex.successor())
-        } else {
-            clusterName = commandName
-            taskName = nil
-        }
-        
-        guard let command = clusterCommands.filter({ $0.cluster.name == clusterName }).first else {
-            throw CLIError.Error("Cluster not found")
-        }
-        
-        command.taskName = taskName
-        
-        return command
-    }
-    
-}
