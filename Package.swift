218839ca816abfa550ba507b4920883f2e0f6e55
Jake Heiser <jakeheiser1@gmail.com>
2016-03-12 09:22:16 -0600

    Updated SwiftCLI

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Flock",
     dependencies: [
-        .Package(url: "/Users/jakeheiser/Documents/Apps/SwiftCLI", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/jakeheis/SwiftCLI", majorVersion: 1, minor: 2),
         .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 1)
     ]
 )
