255603818452eca3e3310538511c435f7ca826b0
Jake Heiser <jakeheiser1@gmail.com>
2016-03-14 20:57:50 -0500

    Paths file

---
+++ Sources/Paths.swift
@@ -0,0 +1,19 @@
+public struct Paths {
+    
+    public static var projectDirectory: String {
+        return "\(Config.deployDirectory)/\(Config.projectName)"
+    }
+    
+    public static var currentDirectory: String {
+        return "\(projectDirectory)/current"
+    }
+    
+    public static var releasesDirectory: String {
+        return "\(projectDirectory)/releases"
+    }
+    
+    public static var executable: String {
+        return "\(currentDirectory)/.build/debug/\(Config.projectName)"
+    }
+    
+}
