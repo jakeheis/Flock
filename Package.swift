a970c1fba6fdcc553af1b26e6f12acc7f3957b24
Jake Heiser <jakeheiser1@gmail.com>
2017-04-22 15:57:02 -0700

    Tests

--- Package.swift
+++ Package.swift
@@ -6,5 +6,8 @@ let package = Package(
         .Package(url: "https://github.com/jakeheis/SwiftCLI", majorVersion: 3, minor: 0),
         .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 2, minor: 0),
         .Package(url: "https://github.com/jakeheis/Spawn", majorVersion: 0, minor: 0)
+    ],
+    exclude: [
+        "Tests/TestProject"
     ]
 )
--- Tests/FlockTests/DeployTests.swift
+++ Tests/FlockTests/DeployTests.swift
@@ -11,21 +11,63 @@ import XCTest
 
 class DeployTests: FlockTestCase {
     
-    func testGit() {
+    func testTasks() {
         guard let testServer = testServer else {
             print("Skipping DeployTests (Docker server not running)")
             return
         }
         
+        Config.deployDirectory = "/var/www"
         Config.projectName = "TestProject"
         Config.repoURL = "/tmp/TestProject"
+        
+        git(testServer)
+        build(testServer)
+        link(testServer)
+    }
+    
+    func git(_ testServer: Server) {
+        XCTAssert(!testServer.directoryExists("/var/www"))
         
         let git = GitTask()
         do {
             try git.run(on: testServer)
         } catch let error {
             XCTFail("Git task should succeed; failed with \(error)")
+            return
         }
+        
+        XCTAssert(testServer.directoryExists("/var/www/TestProject"))
+        XCTAssert(testServer.directoryExists("/var/www/TestProject/next"))
+        XCTAssert(testServer.directoryExists("/var/www/TestProject/releases"))
+    }
+    
+    func build(_ testServer: Server) {
+        XCTAssert(!testServer.fileExists("/var/www/TestProject/next/.build/release/TestProject"))
+        
+        let git = BuildTask()
+        do {
+            try git.run(on: testServer)
+        } catch let error {
+            XCTFail("Build task should succeed; failed with \(error)")
+            return
+        }
+        
+        XCTAssert(testServer.fileExists("/var/www/TestProject/next/.build/release/TestProject"))
+    }
+    
+    func link(_ testServer: Server) {
+        XCTAssert(!testServer.directoryExists("/var/www/TestProject/current"))
+        
+        let git = LinkTask()
+        do {
+            try git.run(on: testServer)
+        } catch let error {
+            XCTFail("Link task should succeed; failed with \(error)")
+            return
+        }
+        
+        XCTAssert(testServer.directoryExists("/var/www/TestProject/current"))
     }
     
 }
