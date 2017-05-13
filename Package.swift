5e189f393fbd1a7717e181c01eafbc2730a63f55
Jake Heiser <jakeheiser1@gmail.com>
2017-05-13 15:48:32 -0700

    Link to nmssh

--- Package.swift
+++ Package.swift
@@ -1,15 +1,17 @@
 import PackageDescription
 
+// Compile with: swift build -Xlinker -lssh2 -Xlinker -L/usr/local/lib/
+
 let package = Package(
     name: "Flock",
     dependencies: [
         .Package(url: "https://github.com/jakeheis/SwiftCLI", majorVersion: 3, minor: 0),
         .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 2, minor: 0),
         .Package(url: "https://github.com/jakeheis/Spawn", majorVersion: 0, minor: 0),
-        .Package(url: "/Users/jakeheiser/Desktop/Projects/Swift/CNMSSH", majorVersion: 0)
+        .Package(url: "https://github.com/jakeheis/CNMSSH", majorVersion: 1, minor: 0)
     ],
     exclude: [
         "Tests/TestProject"
     ]
 )
-//swift build -Xlinker -lssh2 -Xlinker -L/usr/local/lib/
+
