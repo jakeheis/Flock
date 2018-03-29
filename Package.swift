21410384c0ae0045d967d7470dd203a71fcd1f23
Jake Heiser <jakeheiser1@gmail.com>
2018-03-28 22:05:32 -1000

    Separate FlockCLI back out

--- Package.swift
+++ Package.swift
@@ -6,18 +6,14 @@ import PackageDescription
 let package = Package(
     name: "Flock",
     products: [
-        .executable(name: "flock", targets: ["FlockCLI"]),
-        .library(name: "FlockLib", targets: ["FlockLib"]),
+        .library(name: "Flock", targets: ["Flock"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/jakeheis/Beak", from: "0.3.3"),
         .package(url: "https://github.com/onevcat/Rainbow", from: "3.0.0"),
         .package(url: "https://github.com/jakeheis/Shout", from: "0.2.3"),
-        .package(url: "https://github.com/jakeheis/SwiftCLI", from: "4.1.0"),
     ],
     targets: [
-        .target(name: "FlockCLI", dependencies: ["Rainbow", "SwiftCLI", "BeakCore"]),
-        .target(name: "FlockLib", dependencies: ["BeakCore", "Rainbow", "Shout"]),
-        .testTarget(name: "FlockLibTests", dependencies: ["FlockLib"]),
+        .target(name: "Flock", dependencies: ["Rainbow", "Shout"]),
+        .testTarget(name: "FlockTests", dependencies: ["Flock"]),
     ]
 )
